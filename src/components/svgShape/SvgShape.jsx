ece57bd4b9c84cf0aefa8b9d34704002ff13dec1
fahime ghaforifar <fahime.ghaforifar@gmail.com>
2024-01-14 13:47:07 +0330

    add rectangle shape

--- src/components/svgShape/SvgShape.jsx
+++ src/components/svgShape/SvgShape.jsx
@@ -1,9 +1,33 @@
 import React, { Fragment } from "react";
-import "./svgShape.module.css";
+import styled from "./svgShape.module.css";
 function Svgchape() {
   return (
     <Fragment>
-      <svg></svg>
+      <div className={styled.container}>
+        <svg
+          // viewBox="0 0 100 100"
+          width="800"
+          height="500"
+          // x="100"
+          // y="0"
+        >
+          {/* rectangle shape  */}
+          <rect
+            // className={`shape ${isClicked ? "clicked" : ""}`}
+            // x="50"
+            // y="10"
+            width="150"
+            height="50"
+            // fill={selectedShape === "barChart" ? "#fa5f3c" : "blue"}
+            // style={{ cursor: "pointer" }}
+            // onClick={() => handleShapeClick("barChart")}
+          />
+          <text x="45" y="30" fill="#fff">
+            Section B
+          </text>
+        </svg>
+      </div>
     </Fragment>
   );
 }
+export default Svgchape;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import "./home.module.css";
+import Svgchape from "../../components/svgShape/SvgShape";
+function Home() {
+  return (
+    <Fragment>
+      <Svgchape />
+    </Fragment>
+  );
+}
+export default Home;
