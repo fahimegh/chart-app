21e00cf07ded5e03675ea005c9024f6937fc2283
fahime ghaforifar <fahime.ghaforifar@gmail.com>
2024-01-14 22:49:26 +0330

    add onclick event on shapes and draw charts

--- src/components/svgShape/SvgShape.jsx
+++ src/components/svgShape/SvgShape.jsx
@@ -1,42 +1,59 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import styled from "./svgShape.module.css";
-function Svgchape() {
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+function Svgchape({ selectedShape, onShapeClick }) {
   return (
-    <Fragment>
-      <div className={styled.container}>
-        <svg
-          // viewBox="0 0 100 100"
-          width="800"
-          height="500"
-          // x="100"
-          // y="0"
-        >
-          {/* rectangle shape  */}
-          <rect x="275" y="20" width="150" height="50" />
-          <text x="320" y="50" fill="#fff">
-            Section B
-          </text>
-          {/*left trapezoid shape */}
-          <polygon points="200,200 300,230 300,300 200,330 " />
-          <text x="212" y="270" fill="#fff">
-            Section A
-          </text>
-          {/* right trapezoid shape */}
-          <polygon points="400,230 500,200 500,330 400,300" />
-          <text x="420" y="270" fill="#fff">
-            Section C
-          </text>
-          {/* Lines between shapes */}
-          <line x1="275" y1="35" x2="100" y2="35" />
-          <line x1="100" y1="50" x2="100" y2="270" />
-          <line x1="100" y1="270" x2="200" y2="270" />
-          <line x1="300" y1="270" x2="400" y2="270" />
-          <line x1="500" y1="270" x2="600" y2="270" />
-          <line x1="600" y1="270" x2="600" y2="50" />
-          <line x1="600" y1="50" x2="425" y2="50" />
-        </svg>
-      </div>
-    </Fragment>
+    <>
+      {/* <div className={styled.container}> */}
+      <svg
+        // viewBox="0 0 200 200"
+        width="800"
+        height="500"
+        // x="100"
+        // y="0"
+      >
+        {/* rectangle shape  */}
+        <rect
+          x="250"
+          y="20"
+          width="200"
+          height="50"
+          fill={selectedShape === "barChart" ? "#fa5f3c" : "none"}
+          onClick={() => onShapeClick("barChart")}
+        />
+        <text x="320" y="50" fill="#fff">
+          Section B
+        </text>
+        {/*left trapezoid shape */}
+        <polygon
+          points="200,200 300,230 300,300 200,330 "
+          fill={selectedShape === "histogramChart" ? "#fa5f3c" : "none"}
+          onClick={() => onShapeClick("histogramChart")}
+        />
+        <text x="212" y="270" fill="#fff">
+          Section A
+        </text>
+        {/* right trapezoid shape */}
+        <polygon
+          points="400,230 500,200 500,330 400,300"
+          fill={selectedShape === "lineChart" ? "#fa5f3c" : "none"}
+          onClick={() => onShapeClick("lineChart")}
+        />
+        <text x="420" y="270" fill="#fff">
+          Section C
+        </text>
+        {/* Lines between shapes */}
+        <line x1="250" y1="45" x2="100" y2="45" />
+        <line x1="100" y1="45" x2="100" y2="270" />
+        <line x1="100" y1="270" x2="200" y2="270" />
+        <line x1="300" y1="270" x2="400" y2="270" />
+        <line x1="500" y1="270" x2="600" y2="270" />
+        <line x1="600" y1="270" x2="600" y2="45" />
+        <line x1="600" y1="45" x2="450" y2="45" />
+      </svg>
+      {/* </div> */}
+    </>
   );
 }
 export default Svgchape;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,93 @@
-import React, { Fragment } from "react";
-import "./home.module.css";
+import React, { useState, useEffect, Fragment } from "react";
+import styled from "./home.module.css";
 import Svgchape from "../../components/svgShape/SvgShape";
+import axios from "axios";
+import { Bar, Pie, Line, Gauge } from "@ant-design/charts";
+import { Column } from "@ant-design/plots";
+import { Histogram } from "@ant-design/plots";
+import "bootstrap/dist/css/bootstrap.min.css";
 function Home() {
+  const [selectedShape, setSelectedShape] = useState(null);
+  const [chartData, setChartData] = useState(null);
+  // receive data frome local server with axios
+  const fetchData = (chartType) => {
+    axios
+      .get(`http://localhost:4000/${chartType}`)
+      .then((response) => {
+        console.log(response.data.data);
+        setChartData(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  // onclick the svg shapes
+
+  const handleShapeClick = (chartType) => {
+    setSelectedShape(chartType);
+    fetchData(chartType);
+  };
+
   return (
-    <Fragment>
-      <Svgchape />
-    </Fragment>
+    <>
+      <div className={styled.container}>
+        {/* <div style={{ display: "flex" }}> */}
+        <div className="col-md-6">
+          <Svgchape
+            selectedShape={selectedShape}
+            onShapeClick={handleShapeClick}
+          />
+        </div>
+        {chartData && (
+          <div className="col-md-6">
+            {selectedShape === "barChart" && (
+              <Bar
+                data={chartData}
+                xField="label"
+                yField="value"
+                colorField="#56fc03"
+                label={{
+                  style: {
+                    fill: "#FFFFFF",
+                    opacity: 0.6,
+                  },
+                }}
+              />
+            )}
+            {selectedShape === "histogramChart" && (
+              <Histogram
+                data={chartData}
+                // isGroup="true"
+                // xField="type"
+                // yField="value"
+                // seriesField="city"
+                // colorField="#0c6aed"
+
+                binField="value"
+                // theme={customTheme}
+                binWidth="2"
+                colorField="#0c6aed"
+              />
+            )}
+            {selectedShape === "lineChart" && (
+              <Line
+                data={chartData}
+                xField="label"
+                yField="value"
+                colorField="#ddf545"
+                label={{
+                  style: {
+                    fill: "#FFFFFF",
+                    opacity: 0.6,
+                  },
+                }}
+              />
+            )}
+          </div>
+        )}
+        {/* </div> */}
+      </div>
+    </>
   );
 }
 export default Home;
